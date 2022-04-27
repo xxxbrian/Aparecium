ecbf45d8acaaa071530f75b69366bbcda788cd07
Bojin Li <bojin.li@foxmail.com>
2022-04-28 02:17:53 +0800

    version 0.2

---
+++ README.md
@@ -0,0 +1,13 @@
+# APARECIUM
+
+**ap·ar·e·ci·um**
+_/əˈper ē sē ēəm/_
+_noun_
+
+**Aparecium** is the revealing charm, used to reveal hidden messages or other markings which are hidden. It can also counteract a Concealment Charm.
+
+> "She tapped the diary three times and said, "Aparecium!" Nothing happened."
+> — Hermione using the spell on Tom Riddle's diary
+
+**What does the spell aparecium do?**
+The spell aparecium reveal hidden messages or other markings which are hidden or concealed.
---
+++ apareciym.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Apareciym
+// @namespace    https://github.com/xxxbrian/Apareciym
+// @version      0.2
+// @description  The spell aparecium reveal hidden messages or other markings which are hidden or concealed.
+// @author       Bojin Li
+// @match        *://cgi.cse.unsw.edu.au/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=unsw.edu.au
+// @require      https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $(function () {
+        $("#questions").attr("style","");
+    });
+})();
+console.info("Hermione tapped the diary three times and said, Aparecium!")
